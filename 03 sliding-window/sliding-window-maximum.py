f0163f6b0bd2c982725be2f8c23b8b0622f5366a
Pritam Nikam <pritam.nikam@gmail.com>
2025-01-19 21:57:18 +0530

    Solution for sliding window maximum problems.

--- 03 sliding-window/sliding-window-maximum.py
+++ 03 sliding-window/sliding-window-maximum.py
@@ -1,36 +1,36 @@
 from collections import deque
 
-# function to clean up the deque
-def clean_up(i, current_window, nums):
+# Remove all indexes from current window whose corresponding 
+# values are smaller than or equal to current value 
+def cleanup(i, current_window, nums):
     while current_window and nums[i] >= nums[current_window[-1]]:
         current_window.pop()
 
-# function to find the maximum in all possible windows
+# Function to find maximum in all windows
 def find_max_sliding_window(nums, w):
-    if len(nums) < w:
-        return []
-    
     if len(nums) == 1:
         return nums
-
+    
     output = []
     current_window = deque()
 
-    # initialize and calculate for the first window
-    for i in range(0, w):
-        clean_up(i, current_window, nums)
+    # fill the first window elements
+    for i in range(w):
+        cleanup(i, current_window, nums)
         current_window.append(i)
-
-    # top of deque is the max-value in present window
-    output.append(nums[current_window[0]])
     
-    # slide window to the right
-    for i in range(w, len(nums)):
-        clean_up(i, current_window, nums)
-        if current_window and current_window[0] <= (i - w):
-            current_window.popleft()
+    # first element always hold the max in current window
+    output.append(current_window[0])
 
+    # loop existing element in the nums array 
+    for i in range(w, len(nums)):
+        cleanup(i, current_window, nums)
+        # slide window to the right
+        if current_window and current_window[0] <= (i-w):
+            current_window.popleft()
+        
         current_window.append(i)
+        # add the max element to the output aray
         output.append(nums[current_window[0]])
 
     return output

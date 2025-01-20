2b35c986719439fa7e55eee4581d16e71c3b4849
Pritam Nikam <pritam.nikam@gmail.com>
2025-01-20 21:32:32 +0530

    Optimized solution for minimum window subsequence problems.

--- 03 sliding-window/minimum-window-subsequence.py
+++ 03 sliding-window/minimum-window-subsequence.py
@@ -9,22 +9,35 @@ def min_window(str1, str2):
     while index_s1 < size_str1:
         # check if the character pointed by index_s1 in str1
         # is the same as the character pointed by index_s2 in str2
-        if str1[index_s1] == str2[index_s2]:
-            # if this was the first character of str2, mark it as the start of the substring
-            if index_s2 == 0:
-                start = index_s1
-            
-            # if the pointed character is the same in both strings increment index_s2
+        if str1[index_s1] == str2[index_s2]:         
+            # if the pointed character is the same
+            # in both strings increment index_s2
             index_s2 += 1
 
             # check if a valid substring has been found
             if index_s2 == size_str2:
-                end = index_s1
+                # initialize start to the index where all characters of
+                # str2 were present in str1
+                start, end = index_s1, index_s1
+                index_s2 -= 1
 
+                # decrement pointer index_s2 and start a reverse loop
+                while index_s2 >= 0:
+                    # decrement pointer index_s2 until all characters of
+                    #  str2 are found in str1
+                    if str1[start] == str2[index_s2]:
+                        index_s2 -= 1
+                    
+                    # decrement start pointer everytime to find the
+                    # starting point of the required subsequence
+                    start -= 1
+                start += 1
+                
                 # update min_sub_len and min_subsequence if current subsequence is shorter
                 if end - start + 1 < min_sub_len:
                     min_sub_len = end - start + 1
                     min_subsequence = str1[start : end + 1]
+                
                 # set index_s1 to end + 1 to continue checking in str1
                 # after this discovered subsequence
                 # index_s1 = end + 1
